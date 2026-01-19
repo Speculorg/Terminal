42b59bac6b972f233ff3ef95aa5a6c7f83c0ea5a
Speculorg-Dev <187440777+Speculorg-Dev@users.noreply.github.com>
2026-01-19 04:45:23 +0400

    refactor: add logger package with json stdout adapter

--- core/logger/__init__.py
+++ core/logger/__init__.py
@@ -4,4 +4,10 @@ speculorg.terminal.core.logger
 ==============================
 
 Фасад и адаптеры для логирования (Logger, JsonLogger).
+
 """
+
+
+from .logger import Logger
+
+__all__ = ["Logger"]
